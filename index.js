2400050f30e27881803569194ac5255ee3cc43bd
MalikTalha03 <mtnaqshbandi2003@gmail.com>
2023-10-07 23:31:25 +0500

    Created overlay menu and added functionality to close and open in ham menu

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const hamMenu = document.getElementById('hamMenu');
+const overlayMenu = document.querySelector('.overlay-menu');
+const hamMenuOverlay = document.getElementById('hamMenuOverlay'); 
 
 hamMenu.addEventListener('click', () => {
   hamMenu.classList.toggle('active');
+  overlayMenu.classList.toggle('active');
+});
+
+hamMenuOverlay.addEventListener('click', () => {
+  hamMenu.classList.toggle('active');
+  overlayMenu.classList.toggle('active');
 });
