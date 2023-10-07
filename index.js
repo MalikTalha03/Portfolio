08081a38017497fbf5da524045392f1307cb2d32
MalikTalha03 <mtnaqshbandi2003@gmail.com>
2023-10-07 23:42:41 +0500

    Updated overlay menu links to go to sections and disable the menuii

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const hamMenu = document.getElementById('hamMenu');
 const overlayMenu = document.querySelector('.overlay-menu');
 const hamMenuOverlay = document.getElementById('hamMenuOverlay'); 
+const workSection = document.getElementById('workSec');
+const aboutSection = document.getElementById('aboutSec');
+const contactForm = document.getElementById('contactSec');
 
 hamMenu.addEventListener('click', () => {
   hamMenu.classList.toggle('active');
@@ -11,3 +14,12 @@ hamMenuOverlay.addEventListener('click', () => {
   hamMenu.classList.toggle('active');
   overlayMenu.classList.toggle('active');
 });
+
+function closeOverlayMenu() {
+    overlayMenu.classList.remove('active');
+    hamMenu.classList.remove('active');
+}
+
+workSection.addEventListener('click',closeOverlayMenu);
+aboutSection.addEventListener('click',closeOverlayMenu);
+contactForm.addEventListener('click',closeOverlayMenu);
