7e0765dd0b88701f6fdc8c0a35a04c8040a55483
MalikTalha03 <mtnaqshbandi2003@gmail.com>
2023-10-08 12:38:17 +0500

    Added HTML/CSS/JS for overlay cards

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const workSection = document.getElementById('workSec');
 const aboutSection = document.getElementById('aboutSec');
 const contactForm = document.getElementById('contactSec');
 
+// Get all the buttons that open card overlays
+const projectButtons = document.querySelectorAll('.project.see');
+const overlayCards = document.querySelectorAll('.overlay-card');
+
 hamMenu.addEventListener('click', () => {
   hamMenu.classList.toggle('active');
   overlayMenu.classList.toggle('active');
@@ -20,6 +24,23 @@ function closeOverlayMenu() {
     hamMenu.classList.remove('active');
 }
 
-workSection.addEventListener('click',closeOverlayMenu);
-aboutSection.addEventListener('click',closeOverlayMenu);
-contactForm.addEventListener('click',closeOverlayMenu);
+workSection.addEventListener('click', closeOverlayMenu);
+aboutSection.addEventListener('click', closeOverlayMenu);
+contactForm.addEventListener('click', closeOverlayMenu);
+
+// Add click event listeners to each project button
+projectButtons.forEach((button, index) => {
+  button.addEventListener('click', () => {
+    // Show the overlay card associated with the clicked button
+    overlayCards[index].classList.add('active');
+  });
+});
+
+// Add click event listener to close buttons in overlay cards
+const closeButtons = document.querySelectorAll('.close');
+closeButtons.forEach((closeButton) => {
+  closeButton.addEventListener('click', () => {
+    // Hide the overlay card when the close button is clicked
+    closeButton.closest('.overlay-card').classList.remove('active');
+  });
+});
