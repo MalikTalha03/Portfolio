888c00f1876c9666f9d126c296b904fa9d6ee5db
MalikTalha03 <mtnaqshbandi2003@gmail.com>
2023-10-08 14:58:18 +0500

    Fixed Linter errors

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const hamMenu = document.getElementById('hamMenu');
 const overlayMenu = document.querySelector('.overlay-menu');
-const hamMenuOverlay = document.getElementById('hamMenuOverlay'); 
+const hamMenuOverlay = document.getElementById('hamMenuOverlay');
 const workSection = document.getElementById('workSec');
 const aboutSection = document.getElementById('aboutSec');
 const contactForm = document.getElementById('contactSec');
@@ -20,27 +20,23 @@ hamMenuOverlay.addEventListener('click', () => {
 });
 
 function closeOverlayMenu() {
-    overlayMenu.classList.remove('active');
-    hamMenu.classList.remove('active');
+  overlayMenu.classList.remove('active');
+  hamMenu.classList.remove('active');
 }
 
 workSection.addEventListener('click', closeOverlayMenu);
 aboutSection.addEventListener('click', closeOverlayMenu);
 contactForm.addEventListener('click', closeOverlayMenu);
 
-// Add click event listeners to each project button
 projectButtons.forEach((button, index) => {
   button.addEventListener('click', () => {
-    // Show the overlay card associated with the clicked button
     overlayCards[index].classList.add('active');
   });
 });
 
-// Add click event listener to close buttons in overlay cards
 const closeButtons = document.querySelectorAll('.close');
 closeButtons.forEach((closeButton) => {
   closeButton.addEventListener('click', () => {
-    // Hide the overlay card when the close button is clicked
     closeButton.closest('.overlay-card').classList.remove('active');
   });
 });
